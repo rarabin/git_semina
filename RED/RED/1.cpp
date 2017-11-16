a15dddc8d940aa430ec8c03a26a9b4392e2e63e5
Ziaho <rmeosdjeldp9344@gmail.com>
2017-11-16 19:36:28 +0900

    Mod 1.cpp

--- RED/RED/1.cpp
+++ RED/RED/1.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 
 int main() {
-	cout << "학번 : 2017104031 이름 : 채수빈" << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			cout << '$';
+		}
+		cout << endl;
+	}
 	return 0;
 }
